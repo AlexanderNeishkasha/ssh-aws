a5040797030ce916d3088956f48f23f144c9c84a
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2020-01-15 20:59:34 +0200

    Changing environment by argument

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -24,17 +26,76 @@ func main() {
 	}
 	amazonSession := initSession()
 	resp := loadInstances(amazonSession)
-	ips := make(map[string]string, 2)
-	for _, reservation := range resp.Reservations {
+	environment := os.Args[1]
+	fmt.Println(environment)
+	ip, err := ip(environment, resp.Reservations)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var pathToKey string
+	if environment == "stg" {
+		pathToKey = os.Getenv("STAGE_KEY")
+	} else if environment == "prod" {
+		pathToKey = os.Getenv("PROD_KEY")
+	} else {
+		log.Fatal("Invalid environment")
+	}
+	runSsh(ip, pathToKey)
+}
+
+func initSession() *session.Session {
+	region := os.Getenv("AWS_DEFAULT_REGION")
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(region),
+		Credentials: credentials.NewEnvCredentials(),
+	})
+	if err != nil {
+		log.Println("Error with creating aws session")
+		log.Fatal(err)
+	}
+	return sess
+}
+
+func loadInstances(amazonSession *session.Session) *ec2.DescribeInstancesOutput {
+	ec2Client := ec2.New(amazonSession)
+	req, resp := ec2Client.DescribeInstancesRequest(&ec2.DescribeInstancesInput{})
+	if err := req.Send(); err != nil {
+		log.Println("Error sending request")
+		log.Fatal(err)
+	}
+	return resp
+}
+
+func env(environment string, instance *ec2.Instance) string {
+	role, env := false, ""
+	for _, tag := range instance.Tags {
+		if *tag.Key == "role" && *tag.Value == "PhpServer" {
+			role = true
+		}
+		if *tag.Key == "env" && *tag.Value == environment {
+			env = *tag.Value
+		}
+	}
+	if role {
+		return env
+	}
+	return ""
+}
+
+func ip(environment string, reservations []*ec2.Reservation) (ip string, err error) {
+	for _, reservation := range reservations {
 		for _, instance := range reservation.Instances {
-			env := env(instance)
+			env := env(environment, instance)
 			if *instance.State.Code == RunningCode && env != "" {
-				ips[env] = *instance.PrivateIpAddress
+				return *instance.PrivateIpAddress, nil
 			}
 		}
 	}
-	pathToKey := os.Getenv("STAGE_KEY")
-	cmd := exec.Command("ssh", "-i"+pathToKey, "ubuntu@"+ips["stg"])
+	return "", errors.New("IP for env '"+environment+"' not found")
+}
+
+func runSsh(ip string, pathToKey string) {
+	cmd := exec.Command("ssh", "-i"+pathToKey, "ubuntu@"+ip)
 	// Start the command with a pty.
 	ptmx, err := pty.Start(cmd)
 	if err != nil {
@@ -65,43 +126,4 @@ func main() {
 	// Copy stdin to the pty and the pty to stdout.
 	go func() { _, _ = io.Copy(ptmx, os.Stdin) }()
 	_, _ = io.Copy(os.Stdout, ptmx)
-}
-
-func initSession() *session.Session {
-	region := os.Getenv("AWS_DEFAULT_REGION")
-	sess, err := session.NewSession(&aws.Config{
-		Region:      aws.String(region),
-		Credentials: credentials.NewEnvCredentials(),
-	})
-	if err != nil {
-		log.Println("Error with creating aws session")
-		log.Fatal(err)
-	}
-	return sess
-}
-
-func loadInstances(amazonSession *session.Session) *ec2.DescribeInstancesOutput {
-	ec2Client := ec2.New(amazonSession)
-	req, resp := ec2Client.DescribeInstancesRequest(&ec2.DescribeInstancesInput{})
-	if err := req.Send(); err != nil {
-		log.Println("Error sending request")
-		log.Fatal(err)
-	}
-	return resp
-}
-
-func env(instance *ec2.Instance) string {
-	role, env := false, ""
-	for _, tag := range instance.Tags {
-		if *tag.Key == "role" && *tag.Value == "PhpServer" {
-			role = true
-		}
-		if *tag.Key == "env" && (*tag.Value == "prod" || *tag.Value == "stg") {
-			env = *tag.Value
-		}
-	}
-	if role {
-		return env
-	}
-	return ""
 }
