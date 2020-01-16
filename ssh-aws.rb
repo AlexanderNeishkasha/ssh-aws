857808beab2fa6e8f5f0d8c1b8b6f7bb83a25d7e
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2020-01-16 22:58:12 +0200

    Update sha256

--- ssh-aws.rb
+++ ssh-aws.rb
@@ -2,7 +2,7 @@ class SshAws < Formula
   desc "Tool for connection to php servers by private ip"
   homepage "https://github.com/AlexanderNeishkasha/ssh-aws"
   url "https://github.com/AlexanderNeishkasha/ssh-aws/archive/v0.2.0.tar.gz"
-  sha256 "e59bc27fc385583a119756be674aa320d4d4c4146ed60b474c0e1ee084585c54"
+  sha256 "870f0d3d90910c7d59c21411d19998f675f5dca082f150ec5ab4bc13672fbeeb"
 
   depends_on "go" => :build
 
