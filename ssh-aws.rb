250c7d3ff1d4c062f3505916b5491162ccd3daee
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2020-05-19 21:20:06 +0300

    Update checksum for homebrew

--- ssh-aws.rb
+++ ssh-aws.rb
@@ -1,8 +1,8 @@
 class SshAws < Formula
   desc "Tool for connection to php servers by private ip"
   homepage "https://github.com/AlexanderNeishkasha/ssh-aws"
-  url "https://github.com/AlexanderNeishkasha/ssh-aws/archive/v0.2.0.tar.gz"
-  sha256 "870f0d3d90910c7d59c21411d19998f675f5dca082f150ec5ab4bc13672fbeeb"
+  url "https://github.com/AlexanderNeishkasha/ssh-aws/archive/v0.2.1.tar.gz"
+  sha256 "08cb31d398aae881eebd4217aaea9aa09c701d4f167f6d345fccd28c34ffcbbd"
 
   depends_on "go" => :build
 
