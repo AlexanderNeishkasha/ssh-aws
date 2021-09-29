2756cebf32c8d7d2b19c4213c12e0f868206ec07
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2021-09-29 17:15:45 +0300

    Update sha checksum

--- ssh-aws.rb
+++ ssh-aws.rb
@@ -2,7 +2,7 @@ class SshAws < Formula
   desc "Tool for connection to php servers by private ip"
   homepage "https://github.com/AlexanderNeishkasha/ssh-aws"
   url "https://github.com/AlexanderNeishkasha/ssh-aws/archive/v0.3.0.tar.gz" :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "158b3b7ae3fe24f31e84d9bfb7e6b2ed3e5af57948dc0b6da752bb421f3ba1a9"
+  sha256 "613a9f828c2113edf8d21e65d0fdfb7d4e785c520f4183de42d008d9738a9840"
 
   depends_on "go" => :build
 
