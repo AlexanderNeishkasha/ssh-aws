3c857f5cd3c8bacd0d8788aace47e447e7e369bf
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2021-09-29 16:54:16 +0300

    Update file

--- ssh-aws.rb
+++ ssh-aws.rb
@@ -1,7 +1,7 @@
 class SshAws < Formula
   desc "Tool for connection to php servers by private ip"
   homepage "https://github.com/AlexanderNeishkasha/ssh-aws"
-  url "https://github.com/AlexanderNeishkasha/ssh-aws/archive/v0.2.2.tar.gz"
+  url "https://github.com/AlexanderNeishkasha/ssh-aws/archive/v0.2.2.tar.gz" :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "158b3b7ae3fe24f31e84d9bfb7e6b2ed3e5af57948dc0b6da752bb421f3ba1a9"
 
   depends_on "go" => :build
